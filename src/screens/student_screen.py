1c2025a873385cdf43f0cc4e5cb783d335f22350
Ritika Dhakate <ritikadhakate152@gmail.com>
2026-05-16 14:54:59 +0530

    teacher authentication complete

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -1,8 +1,26 @@
 import streamlit as st
-from src.ui.base_layout import style_base_layout
-from src.ui.base_layout import style_background_dashboard
+from src.ui.base_layout import style_background_dashboard, style_base_layout
+from src.components.header import header_dashboard
+from src.components.footer import footer_dashboard
+from src.database.db import check_teacher_exists, create_teacher, teacher_login
 
 def student_screen():
     style_background_dashboard()
     style_base_layout()
-    st.header("Student Screen")
+
+    c1, c2 = st.columns(2, vertical_alignment="center", gap="xxlarge")
+
+    with c1:
+        header_dashboard()
+    with c2:
+        if st.button("Go back to Home", type="secondary", key="loginbackbtn", shortcut="control+backspace"):
+            st.session_state['login_type'] = None
+            st.rerun()
+    
+    st.header("Login using FaceID", text_alignment="center")
+
+    st.space()
+    st.space()
+    
+    st.camera_input("Position your face in the center")
+    footer_dashboard()
